172f98474979327d496e8c8c2840a3b7e717348c
Tejasvi S Jain <tejasvisjain0214@gmail.com>
2023-01-01 19:57:13 +0530

    Create EvaluateReversePolishNotation.java

---
+++ Microsoft.1-5Jan/EvaluateReversePolishNotation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        int var1 = 0;
+        int var2 = 0;
+        for(int i = 0; i < tokens.length; i++){
+            if(tokens[i].equals("+")){
+                var1 = st.pop();
+                var2 = st.pop();
+                st.add(var1+var2);
+            }
+            else if(tokens[i].equals("-")){
+                var1 = st.pop();
+                var2 = st.pop();
+                st.add(var2-var1);
+            }
+            else if(tokens[i].equals("*")){
+                var1 = st.pop();
+                var2 = st.pop();
+                st.add(var2*var1);
+            }
+            else if(tokens[i].equals("/")){
+               var1 = st.pop();
+                var2 = st.pop();
+                st.add(var2/var1);
+            }
+            else{
+                st.add(Integer.parseInt(tokens[i]));
+            }
+        }
+            return st.pop();
+    }
+}
